af7b7b87049c27f58ab808f2de0be02976fe62c8
Lucas Zordan <lucaszordan0204@gmail.com>
2026-04-11 22:52:17 -0300

    feat: find a location block from config file that matches the request, to use in proxy middleware

--- core/proxy-middleware.go
+++ core/proxy-middleware.go
@@ -4,6 +4,8 @@ import (
 	configTypes "go-reverse-proxy/config-file"
 	"go-reverse-proxy/parser"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 type ProxyMiddleware struct {
@@ -18,13 +20,30 @@ func NewProxyMiddleware(configFilePath string) *ProxyMiddleware {
 }
 
 func (m *ProxyMiddleware) Middleware(next http.Handler) http.Handler {
-	// now I can use config from config file
-	// m.config.FindBlocksByName(...)
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		r.Header.Set("X-From-Proxy", "true")
-		block := m.config.FindBlocksByName("http")
-		parser.PrintBlock(block[0], 0)
-		w.Write([]byte("Hello, world!"))
+		//block := m.config.FindBlocksByName("http")
+		//parser.PrintBlock(block[0], 0)
+
+		// get location block that matches request
+		locationBlock := m.findMatchLocation(r.URL)
+		if locationBlock != nil {
+			w.Write([]byte("Proxy passing!"))
+			return
+		}
+
+		w.Write([]byte("Hello from xnign!"))
 	})
+}
+
+func (m *ProxyMiddleware) findMatchLocation(requestLocation *url.URL) *configTypes.Block {
+	locationBlocks := m.config.FindBlocksByName("location")
+	for _, block := range locationBlocks {
+		location := block.Args[0]
+		if strings.HasPrefix(requestLocation.Path, location) {
+			return block
+		}
+	}
+
+	return nil
 }
--- handler/request-handler.go
+++ handler/request-handler.go
@@ -24,8 +24,8 @@ func NewRequestHandler(proxyMiddleware *core.ProxyMiddleware) http.Handler {
 	router.Use(proxyMiddleware.Middleware)
 
 	// this route definition serves to prevent 404 in requests to the server
-	// and does not have implementation because core middleware is responsible
-	// for handling all requests (doing core pass)
+	// and does not have implementation because proxy middleware is responsible
+	// for handling all requests (doing proxy pass)
 	router.Route("/", func(r chi.Router) {})
 
 	return RequestHandler{
